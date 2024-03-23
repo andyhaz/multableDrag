17ad630ef30c70248baaf1395127f6c53e37c537
andrew <andyhaz1970@gmail.com>
2024-03-23 11:01:29 -0500

    Initial Commit

---
+++ multableDrag/multableDragApp.swift
@@ -0,0 +1,17 @@
+//
+//  multableDragApp.swift
+//  multableDrag
+//
+//  Created by andyhaz on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct multableDragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
